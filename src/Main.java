cf9918e8b783496c6245b8963339edcb18863ed9
nusubbotin <nu.subbotin@ya.ru>
2022-09-07 18:53:01 +0300

    Реализация требований по Заданию №2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("task1:");
         var dog = 8;
         var cat = 3.6;
         var paper = 763789;
+
+        System.out.println("dog="+dog);
+        System.out.println("cat="+cat);
+        System.out.println("paper="+paper);
+
+        System.out.println("task2:");
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
 
         System.out.println("dog="+dog);
         System.out.println("cat="+cat);
