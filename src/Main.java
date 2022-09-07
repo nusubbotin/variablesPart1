2b14c3b964054c783b19e980260cc94f57fd032f
nusubbotin <nu.subbotin@ya.ru>
2022-09-07 18:13:23 +0300

    Инициализация проекта + задание 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println("dog="+dog);
+        System.out.println("cat="+cat);
+        System.out.println("paper="+paper);
+    }
+}
