9c45d4ff959f8b00d7309b564aa1257e4eb0bcd3
nusubbotin <nu.subbotin@ya.ru>
2022-09-07 19:20:28 +0300

    Реализация требований по Заданию №5

--- src/Main.java
+++ src/Main.java
@@ -36,5 +36,18 @@ public class Main {
 
         friend = friend / 7;
         System.out.println("friend="+friend);
+
+        System.out.println("task5:");
+        var frog = 3.5;
+        System.out.println("frog="+frog);
+
+        frog = frog * 10;
+        System.out.println("frog="+frog);
+
+        frog = frog / 3.5;
+        System.out.println("frog="+frog);
+
+        frog = frog + 4;
+        System.out.println("frog="+frog);
     }
 }
